a49e318ab43844b24016739b711a24f317b59994
Imran Iqbal <imran1337@gmail.com>
2019-08-11 19:15:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,13 +7,12 @@ function theBeatlesPlay(musicians, instruments) {
   return allMusicians;
 }
 
-function johnLennonFacts() {
-    var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
-    var newFacts = [];
-    var i = 0;
-    while (i < facts.length) {
-        newFacts.push(facts[i] +"!!!");
-        i++;
-    }
-    return newFacts;
+function johnLennonFacts(facts){
+  let addArray = [];
+
+   for(let i=0; i<facts.length; ++i){
+    addArray.push(facts[i]+"!!!");
+  }
+
+   return addArray;
 }
