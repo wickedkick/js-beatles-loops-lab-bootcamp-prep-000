1d7bd3933715f8b7ce3ffdcbf67b2fada68aefce
Imran Iqbal <imran1337@gmail.com>
2019-08-11 19:12:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,14 +8,12 @@ function theBeatlesPlay(musicians, instruments) {
 }
 
 function johnLennonFacts() {
-  var facts = [
-'He was the last Beatle to learn to drive', 'He was never a vegetarian', 'He was a choir boy and boy scout', 'He hated the sound of his own voice'
-];
-var newFacts = [];
-var i = 0;
-while ( i < facts.length ) {
-  newFacts.push(facts[i] + "!!!");
-  i++;
-}
-return newFacts;
+    var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
+    var newFacts = [];
+    var i = 0;
+    while (i < facts.length) {
+        newFacts.push(facts[i] +"!!!");
+        i++;
+    }
+    return newFacts;
 }
