cfb219b2a28a241be68919a242ea4290f57de445
Imran Iqbal <imran1337@gmail.com>
2019-08-11 19:11:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,10 +9,7 @@ function theBeatlesPlay(musicians, instruments) {
 
 function johnLennonFacts() {
   var facts = [
-  "He was the last Beatle to learn to drive",
-  "He was never a vegetarian",
-  "He was a choir boy and boy scout",
-  "He hated the sound of his own voice"
+'He was the last Beatle to learn to drive', 'He was never a vegetarian', 'He was a choir boy and boy scout', 'He hated the sound of his own voice'
 ];
 var newFacts = [];
 var i = 0;
